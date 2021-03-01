bb98fadc1464a4f7e4ceed20254b7be83e9668e9
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-03-01 21:22:54 +0000

    Vidly project with Mosh

---
+++ vidly/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Movies from './components/movies'
+
+function App() {
+  return (
+    <main className="container">
+      <h1><Movies/></h1>
+    </main>
+  );
+}
+
+export default App;
