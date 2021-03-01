64e16b3624ffcd4b9b785273d90171798ead9784
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-03-01 22:18:51 +0000

    Movies have displayed in a table from another file

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -5,8 +5,22 @@ class Movies extends Component {
     state = {
          movies: getMovies()
     };
+
+    handleDelete = (movie) =>{
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies})
+    };
+
     render() { 
+
+        const {length: count} = this.state.movies;
+
+        if(count === 0) return <p>There are no more Movie in the database.</p>;
+
         return (
+        <div>
+        <p>Showing {count} movies in the database</p>
+        
         <table className="table">
             <thead>
                 <tr>
@@ -14,19 +28,27 @@ class Movies extends Component {
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                     {this.state.movies.map(movie => (
-                    <tr>
+                    <tr key={movie._id}>
                         <td>{movie.title}</td>
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
+                        <td>
+                        <button
+                         onClick={() => this.handleDelete(movie)}
+                         className="btn btn-danger btn-sm"> Delete
+                         </button>
+                        </td>
                     </tr>
-                    ))};
+                    ))}
             </tbody>
         </table> 
+        </div>
         );
     }
 }
